5529387bf77e392e2f683ceda5cffbd6504acac4
Rafaesp <rafaespillaque@gmail.com>
2011-07-22 21:46:26 +0200

    Borrada la parte de seekbars del adapter

--- src/com/bunkerdev/chooser/ExpLAdapter.java
+++ src/com/bunkerdev/chooser/ExpLAdapter.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 
 public class ExpLAdapter extends BaseExpandableListAdapter {
 	
-	private ArrayList<SeekBar> bars;
 	private ArrayList<LinearLayout> groups;
 	private ArrayList<LinearLayout> children;
 	private LayoutInflater inflater;
@@ -27,7 +26,6 @@ public class ExpLAdapter extends BaseExpandableListAdapter {
 	public ExpLAdapter(ArrayList<Choice> choices, LayoutInflater inf){
 		groups = new ArrayList<LinearLayout>();
 		children = new ArrayList<LinearLayout>();
-		bars = new ArrayList<SeekBar>();
 		inflater = inf;
 		refreshChoices(choices);
 	}
@@ -72,43 +70,6 @@ public class ExpLAdapter extends BaseExpandableListAdapter {
 
 	public boolean isChildSelectable(int groupPosition, int childPosition) {
 		return true;
-	}
-	
-	public void addChoiceSeekBar(String name){
-		LinearLayout adapterGroup = (LinearLayout) inflater.inflate(R.layout.adapter_group, null);
-		LinearLayout adapterChild = (LinearLayout) inflater.inflate(R.layout.adapter_child_seekbar, null);
-		
-		TextView choiceNameView = (TextView) adapterGroup.findViewById(R.id.choice); 
-		choiceNameView.setText(name);
-		
-		TextView weight = (TextView) adapterChild.findViewById(R.id.choiceWeight);
-		weight.setText(2+"");
-		
-		SeekBar sb = (SeekBar) adapterChild.findViewById(R.id.seekbar);
-		sb.setMax(3);
-		sb.setProgress(2);
-		bars.add(sb);
-		
-		sb.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				//refreshProgress(seekBar);
-				notifyDataSetChanged();
-			}
-			
-			public void onStartTrackingTouch(SeekBar seekBar) {
-			}
-			
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				TextView weight = (TextView) ((View)seekBar.getParent()).findViewById(R.id.choiceWeight);
-				weight.setText(seekBar.getProgress()+"");
-			}
-		});
-		
-		groups.add(adapterGroup);
-		children.add(adapterChild);
-		notifyDataSetChanged();
 	}
 	
 	public void addChoiceRadioGroup(Choice c){
@@ -165,51 +126,4 @@ public class ExpLAdapter extends BaseExpandableListAdapter {
 		}
     }
 
-	
-	
-	/*
-	   private void refreshProgress(SeekBar bar){
-		Integer nProgress = bar.getProgress();
-		Integer oldProgress = (Integer) bar.getTag();
-		
-		boolean increased = nProgress>=oldProgress? true:false;
-		Integer progress = Math.abs(nProgress - oldProgress);
-		
-		Integer change = 0;
-		
-		Integer progressPerBar = ((Double)Math.floor(progress/(bars.size()-1))).intValue();
-		Integer rest = progress%(bars.size()-1);
-		Log.i(tag, "nProg: "+nProgress+" oldProg: "+oldProgress+" incres: "+increased+" progPerBar: "+progressPerBar+" rest: "+rest);
-		if(increased){
-			for(int i = 0; i<bars.size(); i++){
-				SeekBar ibar = bars.get(i);
-				if(!bar.equals(ibar)){
-					if(i<rest){
-						change = ibar.getProgress()-progressPerBar-1;
-						ibar.setProgress(change);
-					}else{
-						change = ibar.getProgress()-progressPerBar;
-						ibar.setProgress(change);
-					}
-					
-					ibar.setTag(change);
-				}
-			}
-		}else{
-			for(int i = 0; i<bars.size(); i++){
-				SeekBar ibar = bars.get(i);
-				if(!bar.equals(ibar)){
-					if(i<rest){
-						change = ibar.getProgress()+progressPerBar+1;
-						ibar.setProgress(change);
-					}else{
-						change = ibar.getProgress()+progressPerBar;
-						ibar.setProgress(change);
-					}
-					ibar.setTag(change);
-				}
-			}	
-		}
-	}
-	*/
 }
