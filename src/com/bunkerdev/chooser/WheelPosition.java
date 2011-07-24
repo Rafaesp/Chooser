83a2bc0f142790b60fa33bfd5c134df3296979b5
Rafaesp <rafaespillaque@gmail.com>
2011-07-24 12:34:44 +0200

    Inicializada la actividad y creada la clase Renderer

--- src/com/bunkerdev/chooser/WheelPosition.java
+++ src/com/bunkerdev/chooser/WheelPosition.java
@@ -1,7 +1,21 @@
 package com.bunkerdev.chooser;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
 
 public class WheelPosition extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		GLSurfaceView view = new GLSurfaceView(this);
+		view.setRenderer(new OpenGLRenderer());
+		setContentView(view);
+		    
+	}
+	
+	
+	
 }
